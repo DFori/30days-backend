08b7020f5660f795488c6bc593c27c6a27fed7fa
DFori <daniel.tatama@binghamuni.edu.ng>
2024-07-02 23:06:09 +0100

    day 2

---
+++ day2/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import chalkAnimation from "chalk-animation";
+
+async function welcome() {
+  let message = chalkAnimation.rainbow("Day 2 of gdsc 30 days of code");
+}
+welcome();
