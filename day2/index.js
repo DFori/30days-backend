4d854e981bf51f5b7281fe93e6452871b3771e5e
DFori <daniel.tatama@binghamuni.edu.ng>
2024-07-03 13:51:11 +0100

    modified index.js

--- day2/index.js
+++ day2/index.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
 
 async function welcome() {
   let message = chalkAnimation.rainbow("Day 2 of gdsc 30 days of code");
